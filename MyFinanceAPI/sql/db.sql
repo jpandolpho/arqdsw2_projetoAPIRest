a84722b202a4b66a24a9ef33070f7f91064ed3af
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-07-14 12:00:20 -0300

    Definindo banco de dados da API.

--- MyFinanceAPI/sql/db.sql
+++ MyFinanceAPI/sql/db.sql
@@ -1,1 +1,18 @@
+CREATE DATABASE IF NOT EXISTS MyFinanceAPI;
 
+USE MyFinanceAPI;
+
+CREATE TABLE categoria (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE transacao (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(150) NOT NULL,
+    tipo ENUM('RECEITA', 'DESPESA') NOT NULL,
+    data_transacao DATETIME NOT NULL,
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+        ON DELETE BLOCK
+);
